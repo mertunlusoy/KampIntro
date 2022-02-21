34fdcbffddf4ce9cf36fe5a13df9c5f86be6ad26
Mert Ünlüsoy <unlusoymert@outlook.com>
2022-02-21 13:36:09 +0300

    4. Gün Sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 6,
+                ProductName = "Kalem",
+                UnitPrice = 35
+            };
+
+            //PascalCase    //camelCase
+            ProductManager productManager = new ProductManager();
+            // ProductManager classında oluşturulan productManager değişkeni,
+            // yeni bir referans numarası almış ProductManager'dır.
+            // Stack(eşitliğin sol tarafı) --- Heap (eşitliğin sağ tarafı)
+
+            productManager.Add(product1);
+
+
+
+        }
+    }
+}
---
+++ ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ValueAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            // sayi 1 = 30 olacaktır.
+
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] = 999 olacaktır. 
+
+            //int,decimal,float, double,bool = değer tiplerdir.
+
+            //array,class,interface = referans tiplerdir.
+
+            //Bellekte stack ve heap adlı kısımlar. Stack'te değer tipler sayi1 = sayi2 dediğimizde
+            //sayi1'in değeri sayi2'nin değerine eşittir denir ve sayi2 ile olan tüm bağ bu noktadan
+            //sonra kopar.
+
+            //Referans tipler int[] sayilar1 dediğimizde stackte sayilar1 adlı bir değişken oluşturur.
+            //New dediğimiz anda heap'te [10,20,30] adlı bir dizi oluşturur. Bellekte adreste tutulur.
+            //Yani new demek, bellekte adres oluştur demektir.
+            //sayilar1 = sayilar2 dediğimizde de sayilar1'in referans numarası = sayılar2'nin referans numarasıdır.
+            //Yani sayilar1'in dizi adresi bellekte 101 ise ve sayilar2 102 ise; sayilar2'nin adresi olan 102'yi
+            //sayilar1'in yeni adresi yapar. Adres kopyalaması yapar. Bu olay C'de pointer denebilir.
+        }
+    }
+}
